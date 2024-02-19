834e839966b78946973c67cc8024fa1f5edf0722
toaaa <toa@toaaa.de>
2024-02-20 00:49:48 +0100

    Skybase

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
-# Europe Times
+# Skybase for Outer Wilds
 
-Replaces end times with The Final Countdown by Europe
+Replaces End Times from Outer Wilds with [**Skybase**](https://www.youtube.com/watch?v=MinVUPVp_uc) by [Standart Skill](https://www.youtube.com/@StandartSkill) feat. Ayanda
 
-Credit to GoldenCrepe on discord for having the mod idea and creating the sound for me!
+Credit to [@CheesyKirah](https://github.com/CheesyKirah) for having the mod idea and to [@MegaPiggy](https://github.com/MegaPiggy) for creating the original mod [EuropeTimes](https://github.com/MegaPiggy/EuropeTimes)!
 
-![Europe Times](https://user-images.githubusercontent.com/34462599/213903580-d8735627-a2c6-4a0b-bc92-822f99ec8ff5.png)
-
-
-<video src="https://user-images.githubusercontent.com/34462599/211179377-961f1375-08a8-41fd-b8c1-d4676eea1150.mov" data-canonical-src="https://user-images.githubusercontent.com/34462599/211179377-961f1375-08a8-41fd-b8c1-d4676eea1150.mov" controls="controls" class="d-block rounded-bottom-2 border-top width-fit" style="max-height:640px; min-height: 200px" autoplay/>
+<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/MinVUPVp_uc?si=wTRqkbiPOINkwJQH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen autoplay></iframe>
---
+++ SkybaseTimes.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace SkybaseTimes
+{
+    public class SkybaseTimes : ModBehaviour
+    {
+        private static SkybaseTimes instance;
+        private const int EndOfTime = (int)AudioType.EndOfTime;
+        private const float DefaultVolume = 0.2f;
+
+        private static SkybaseTimes Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<SkybaseTimes>();
+                }
+                return instance;
+            }
+        }
+
+        private void Awake()
+        {
+            instance = this;
+        }
+
+        private void Start()
+        {
+            ModHelper.Console.Log($"{nameof(SkybaseTimes)} has loaded!");
+            ReplaceInAudioManager();
+            ReplaceInAudioLibrary();
+        }
+
+        private static void ReplaceAudioEntry(Dictionary<int, AudioLibrary.AudioEntry> dictionary)
+        {
+            if (dictionary.TryGetValue(EndOfTime, out var audioEntry))
+            {
+                audioEntry = new AudioLibrary.AudioEntry(AudioType.EndOfTime, GetClips(), DefaultVolume);
+            }
+            else
+            {
+                dictionary.Add(EndOfTime, new AudioLibrary.AudioEntry(AudioType.EndOfTime, GetClips(), DefaultVolume));
+            }
+        }
+
+        public static void ReplaceInAudioManager()
+        {
+            ReplaceAudioEntry(Instance.ModHelper.AudioManager.AudioLibraryDict);
+        }
+
+        public static void ReplaceInAudioLibrary()
+        {
+            ReplaceAudioEntry(Instance.ModHelper.AudioManager.AudioLibraryDict);
+        }
+
+        private static AudioClip[] GetClips() => new AudioClip[1] { GetClip() };
+
+        private static AudioClip GetClip()
+        {
+            return instance.ModHelper.Assets.GetAudio("Skybase_Times.mp3");
+        }
+    }
+}
